6ec3805554e311d924d52dd9222944150f6db35b
CainLuo <cain.jh.luo@gtomato.com>
2026-03-19 19:23:53 +0800

    Update codes

--- core/src/main/ets/models/index.ts
+++ core/src/main/ets/models/index.ts
@@ -1,2 +1,4 @@
 export { GameDataRow, GameDataSummary, DataType, GameId } from "./GameDataRow";
 export { GameStatsRow, ScoredEntity, StatKey } from "./GameStatsRow";
+export { SyncMetaRow, SyncStatus } from "./SyncMetaRow";
+export { SyncMetaRow, SyncStatus } from "./SyncMetaRow";
